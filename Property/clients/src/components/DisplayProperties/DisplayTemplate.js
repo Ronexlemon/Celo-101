da90091b4e3011946094d5026831f740c7d5105e
RonexLemon <ronexondimu@gmail.com>
2022-10-04 16:32:11 +0300

    modify the property page

--- Property/clients/src/components/DisplayProperties/DisplayTemplate.js
+++ Property/clients/src/components/DisplayProperties/DisplayTemplate.js
@@ -1,26 +1,27 @@
 import React from "react"
+import "./display_module.css"
 
 const DisplayTemplate =(props) =>{
     return <div className="card-container">
-    <h1 id='available-tenders-page-h1'>Available Tenders</h1>
+    <h1 id='available-tenders-page-h1'>Available Properties</h1>
     <hr id='horizontal-line' />
-    {props.properties.map((tender, index) => (
-      <div className='tenderCard' key={tender.index} >
+    {props.properties.map((property, index) => (
+      <div className='PropertyCard' key={property.index} >
         <div className='tenderCardHeader' id='tenderCardHeader'>
           <div className="tenderCardHeader-blank"></div>
           <div className="tenderCardHeader-content">
-
-            <h4 id='company-name'> &nbsp;{tender.name}</h4>
-            <p id='tender-description' className='tender-amount-description-p'>{tender.descriptions}</p>
-            <p id='tender-amount' className='tender-amount-description-p'>Tender worth: {tender.price / 1000000000000}</p>
+          <img src={property.image} alt="Transparent Red Jeep Clipart - Audi R8 Car Png, Png Download@kindpng.com"/>
+            
+            <p id='tender-description' className='tender-amount-description-p'>{property.descriptions}</p>
+            <p id='tender-amount' className='tender-amount-description-p'>Proprty worth: {property.price / 1000000000000}</p>
           </div>
         </div>
         <div className='tenderCard-middle' id='tendercard-middle'>
-          <h5>&nbsp;{tender.description}&emsp;&emsp;&emsp;&emsp;&nbsp;{tender.image} </h5>
+          <h5>&nbsp;{property.description}&emsp;&emsp;&emsp;&emsp;&nbsp; </h5>
         </div>
         <div className='bid-btn' id='bid-btn-approve-btn'>
 
-          <button className='btn-bid'  id='btn-bid' >BID</button>
+          <button className='btn-bid'  id='btn-bid' >BUY</button>
 
         </div>
 
